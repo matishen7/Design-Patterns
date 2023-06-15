ba54d675faaa81107d8648fca0f98d3a7a6863ad
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-15 15:50:40 -0500

    CatalogItemBuilder

--- Builder/CatalogBuilderProgram.cs
+++ Builder/CatalogBuilderProgram.cs
@@ -13,20 +13,42 @@ namespace Design_Patterns.Builder
     {
         public class CatalogItem
         {
-            public string Name { get; set; }
-            public string Description { get; set; }
+            public string? Name { get; set; }
+            public string? Description { get; set; }
         }
 
         public class Category : CatalogItem
         {
-            public List<Category> Categories { get; set; }
-            public List<Product> Products { get; set; }
+            public List<Category>? Categories { get; set; }
+            public List<Product>? Products { get; set; }
         }
 
         public class Product : CatalogItem
         {
             public double Price { get; set; }
             public bool Availability { get; set; }
+        }
+
+        public class CatalogItemBuilder<T> where T : CatalogItem, new()
+        {
+            protected T? catalofItem;
+
+            public CatalogItemBuilder()
+            {
+                catalofItem = new T();
+            }
+
+            public CatalogItemBuilder<T> SetName(string name)
+            {
+                catalofItem.Name = name;
+                return this;
+            }
+
+            public CatalogItemBuilder<T> SetDescription(string desc)
+            {
+                catalofItem.Description = desc;
+                return this;
+            }
         }
     }
 
