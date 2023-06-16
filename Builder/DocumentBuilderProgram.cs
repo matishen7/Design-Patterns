ff33e076d6a9a9f3af7ffe9a3bed950d28d9eea3
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-16 13:00:59 -0500

    DocumentBuilder

--- Builder/DocumentBuilderProgram.cs
+++ Builder/DocumentBuilderProgram.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using static Design_Patterns.Builder.DocumentBuilderProgram;
+using Document = Design_Patterns.Builder.DocumentBuilderProgram.Document;
 
 namespace Design_Patterns.Builder
 {
@@ -12,17 +15,74 @@ namespace Design_Patterns.Builder
     /// </summary>
     internal class DocumentBuilderProgram
     {
-        class Document
+        public class Document
         {
             public string? Title { get; set; }
             public string? Content { get; set; }
             public DateTime CreatedDate { get; set; }
-            public string?  Author { get; set; }
+            public string? Author { get; set; }
 
             public override string ToString()
             {
                 return $"{nameof(Title)}: {Title}, {nameof(Content)}: {Content}, {nameof(CreatedDate)}: {CreatedDate}, {nameof(Author)}: {Author}";
             }
         }
+
+        public interface ISetTitle
+        {
+            public ISetContent SetTitle(string title);
+        }
+
+        public interface ISetContent
+        {
+            public IBuild SetContent(string content);
+        }
+
+        public interface IBuild
+        {
+            public Document Build();
+        }
+
+        public class DocumentBuilder
+        {
+            public static ISetTitle Create()
+            {
+                return new Impl();
+            }
+        }
+
+        private class Impl : ISetTitle, ISetContent, IBuild
+        {
+            private Document d = new Document();
+
+            public ISetContent SetTitle(string title)
+            {
+                d.Title = title;
+                return this;
+            }
+
+            public IBuild SetContent(string content)
+            {
+                d.Content = content;
+                return this;
+            }
+
+            public Document Build()
+            {
+                return d;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var d = DocumentBuilder.Create()
+                .SetTitle("D1")
+                .SetContent("asjkhh")
+                .Build();
+            
+            Console.Write(d.ToString());
+        }
     }
+
+
 }
