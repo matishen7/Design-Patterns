b30fcd6bf0b4df3f2e2f865bfbd0cfcadc23b1a7
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-24 22:40:51 -0500

    IVehicleFactory

--- Factories/AbstractVehicleFactoryDemo.cs
+++ Factories/AbstractVehicleFactoryDemo.cs
@@ -8,5 +8,62 @@ namespace Design_Patterns.Factories
 {
     internal class AbstractVehicleFactoryDemo
     {
+        internal interface IVehicle
+        {
+            public void Drive();
+        }
+
+        class Car : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("I am driving a car.");
+            }
+        }
+
+        class Motorcycle : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("I am driving a motorbike.");
+            }
+        }
+
+        class Bicycle : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("I am driving a bicycle.");
+            }
+        }
+
+        interface IVehicleFactory
+        {
+            public IVehicle CreateVehicle();
+        }
+
+        class CarFactory : IVehicleFactory
+        {
+            public IVehicle CreateVehicle()
+            {
+                return new Car();
+            }
+        }
+
+        class MotorcycleFactory : IVehicleFactory
+        {
+            public IVehicle CreateVehicle()
+            {
+                return new Motorcycle();
+            }
+        }
+
+        class BicycleFactory : IVehicleFactory
+        {
+            public IVehicle CreateVehicle()
+            {
+                return new Bicycle();
+            }
+        }
     }
 }
