8757de52a0c3e472e1e24fd99c4d5a0ac7bb206f
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-27 17:14:57 -0500

    CopyConstructorDemo

--- Prototypes/CopyConstructorDemo.cs
+++ Prototypes/CopyConstructorDemo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using static Design_Patterns.Factories.FactoryCodingExercise;
 
 namespace Design_Patterns.Prototypes
@@ -11,7 +13,7 @@ namespace Design_Patterns.Prototypes
     {
         class Person
         {
-            public string[] Names { get; set; }
+            public string Names { get; set; }
             public Address Address { get; set; }
 
             public Person()
@@ -21,14 +23,19 @@ namespace Design_Patterns.Prototypes
 
             public Person(Person other)
             {
-                this.Names = other.Names;
-                this.Address = other.Address;
+                Names = other.Names;
+                Address = new Address(other.Address);
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Names)}: {Names}, {nameof(Address)}: {Address}";
             }
         }
 
         public class Address
         {
-            public string[] StreetName { get; set; }
+            public string StreetName { get; set; }
             public int Number;
 
             public Address()
@@ -38,17 +45,23 @@ namespace Design_Patterns.Prototypes
 
             public Address(Address other)
             {
-                this.Number = other.Number;
-                this.StreetName = other.StreetName;
+                Number = other.Number;
+                StreetName = other.StreetName;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(StreetName)}: {StreetName}, {nameof(Number)}: {Number}";
             }
         }
 
 
         static void Main(string[] args)
         {
-            var john = new Person() { Names = new string[] { "John", "Smith" }, Address = new Address() { StreetName = new string[] { "London street" }, Number = 123 } };
-            var john = new Person() { Names = new string[] { "John", "Smith" }, Address = new Address() { StreetName = new string[] { "London street" }, Number = 123 } };
-
+            var john = new Person() { Names = "John" , Address = new Address() { StreetName = "London street" , Number = 123 } };
+            var jane = new Person(john);
+            Console.WriteLine(jane.ToString());
+            Console.WriteLine(john.ToString());
         }
     }
 }
