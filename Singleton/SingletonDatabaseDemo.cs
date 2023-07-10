3c30b0d8261f39dcf63db1ef2636c275fad99f5d
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-07-10 14:52:48 -0500

    ConfigurableRecordFinder

--- Singleton/SingletonDatabaseDemo.cs
+++ Singleton/SingletonDatabaseDemo.cs
@@ -48,6 +48,38 @@ namespace Design_Patterns.Singleton
                     return result;
                 }
             }
+
+            public class ConfigurableRecordFinder
+            {
+                private IDatabase database;
+
+                public ConfigurableRecordFinder(IDatabase database)
+                {
+                    this.database = database;
+                }
+
+                public int GetTotalPopulation(IEnumerable<string> names)
+                {
+                    int result = 0;
+                    foreach (var name in names)
+                        result += database.GetPopulation(name);
+                    return result;
+                }
+            }
+
+            public class DummyDatabase : IDatabase
+            {
+                public int GetPopulation(string name)
+                {
+                    return new Dictionary<string, int>
+                    {
+                        ["alpha"] = 1,
+                        ["beta"] = 2,
+                        ["gamma"] = 3
+                    }[name];
+                }
+            }
+
             public static void Main()
             {
                 var db = SingletonDatabase.Instance;
