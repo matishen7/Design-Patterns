b19f74daa59b1095a5c9a7cc396fc86e5f8ba8f4
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-07-10 15:04:05 -0500

    MonoStateDemo

--- Singleton/SingletonDatabaseDemo.cs
+++ Singleton/SingletonDatabaseDemo.cs
@@ -80,16 +80,16 @@ namespace Design_Patterns.Singleton
                 }
             }
 
-            public static void Main()
-            {
-                var db = SingletonDatabase.Instance;
+            //public static void Main()
+            //{
+            //    var db = SingletonDatabase.Instance;
 
-                // works just fine while you're working with a real database.
-                var city = "Tokyo";
-                Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+            //    // works just fine while you're working with a real database.
+            //    var city = "Tokyo";
+            //    Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
 
-                // now some tests
-            }
+            //    // now some tests
+            //}
 
         }
     
