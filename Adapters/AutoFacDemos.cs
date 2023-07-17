bb0721cc096e5a44afb083872aa182921f189f25
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-07-17 15:17:34 -0500

    BrdigeDemo

--- Adapters/AutoFacDemos.cs
+++ Adapters/AutoFacDemos.cs
@@ -79,35 +79,35 @@ namespace Design_Patterns.Adapters
 
         public class Adapters
         {
-            static void Main(string[] args)
-            {
-                // for each ICommand, a ToolbarButton is created to wrap it, and all
-                // are passed to the editor
-                var b = new ContainerBuilder();
-                b.RegisterType<OpenCommand>()
-                  .As<ICommand>()
-                  .WithMetadata("Name", "Open");
-                b.RegisterType<SaveCommand>()
-                  .As<ICommand>()
-                  .WithMetadata("Name", "Save");
-                //b.RegisterType<Button>();
-                b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd, ""));
-                b.RegisterAdapter<Meta<ICommand>, Button>(cmd =>
-                  new Button(cmd.Value, (string)cmd.Metadata["Name"]));
-                b.RegisterType<Editor>();
-                using (var c = b.Build())
-                {
-                    var editor = c.Resolve<Editor>();
-                    editor.ClickAll();
+            //static void Main(string[] args)
+            //{
+            //    // for each ICommand, a ToolbarButton is created to wrap it, and all
+            //    // are passed to the editor
+            //    var b = new ContainerBuilder();
+            //    b.RegisterType<OpenCommand>()
+            //      .As<ICommand>()
+            //      .WithMetadata("Name", "Open");
+            //    b.RegisterType<SaveCommand>()
+            //      .As<ICommand>()
+            //      .WithMetadata("Name", "Save");
+            //    //b.RegisterType<Button>();
+            //    b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd, ""));
+            //    b.RegisterAdapter<Meta<ICommand>, Button>(cmd =>
+            //      new Button(cmd.Value, (string)cmd.Metadata["Name"]));
+            //    b.RegisterType<Editor>();
+            //    using (var c = b.Build())
+            //    {
+            //        var editor = c.Resolve<Editor>();
+            //        editor.ClickAll();
 
-                    // problem: only one button
+            //        // problem: only one button
 
-                    foreach (var btn in editor.Buttons)
-                        btn.PrintMe();
+            //        foreach (var btn in editor.Buttons)
+            //            btn.PrintMe();
 
 
-                }
-            }
+            //    }
+            //}
         }
     }
 }
